f5944e7cb7ad613894de0ddfa8a82ab282ae473d
unknown <systen3@gmail.com>
2022-06-17 14:25:48 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo512.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,27 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>
+        Felipe de Souza
+        </h2>
+        <h4>
+        Desenvolvedor Full Stack   </h4>
+        <p>ReactJS | React Native | NodeJS | Javascript | Typescript</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://web.whatsapp.com/send?phone=5573988433532"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Open in WhatsApp
+        </a>
+        <a
+          className="App-link"
+          href="https://portf-lio-pessoal-kkpbnbydj-felipesysten.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open in Portfólio
         </a>
       </header>
     </div>
